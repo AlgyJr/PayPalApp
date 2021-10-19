f871cbbeddd1cd1d93eda2e366595c4787ea61b7
ALgy Aly <algymussa@gmail.com>
2021-10-19 10:19:02 +0200

    Initial commit

--- PayPalApp/ViewController.swift
+++ PayPalApp/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var balanceView: UIView!
+    
+    @IBOutlet weak var getMoreView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getMoreView.layer.cornerRadius      = CGFloat(5.0)
+        
+        balanceView.layer.cornerRadius      = CGFloat(5.0)
+        balanceView.layer.shadowOpacity     = Float(0.5)
+        balanceView.layer.shadowOffset      = CGSize(width: 2.0, height: 1.0)
+        balanceView.layer.shadowRadius      = CGFloat(2.0)
+        
+        
+        //MARK: Navigation Bar
+//        rightBarItemImage = rightBarItemImage?.withRenderingMode(.alwaysOriginal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "settings_icon"), style: .plain, target: nil, action: nil)
+        
+        let profileImageView = UIImageView(image: UIImage(named: "profile_image"))
+        self.navigationItem.titleView = profileImageView
+        
+//        leftBarItemImage = leftBarItemImage?.withRenderingMode(.alwaysOriginal)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "notifications_icon"), style: .plain, target: nil, action: nil)
     }
-
-
 }
-
